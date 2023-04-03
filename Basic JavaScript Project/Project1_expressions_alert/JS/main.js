05ecca2c85ab2a65e5370d95d3133a644d7b0492
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-03 11:15:51 -0500

    Creating Java Code

---
+++ Basic JavaScript Project/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+
+
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text") .innerHTML = str;
+}
+
+var Family= "The Machados", Dad = "John", Mom = "Cristiane", Sons = "John Jr and Jean", Daughters = "Julia and Catherine"
+document.write(Dad); //writing out dad//
+
+document.write(3 + 3); //Adding 3 + 3 togther//
