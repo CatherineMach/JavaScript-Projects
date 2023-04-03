9c67ba087751ce16c76749d6720feb66f582b806
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-03 11:21:35 -0500

    Update main.js

--- Basic JavaScript Project/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Project/Project1_expressions_alert/JS/main.js
@@ -9,3 +9,13 @@ var Family= "The Machados", Dad = "John", Mom = "Cristiane", Sons = "John Jr and
 document.write(Dad); //writing out dad//
 
 document.write(3 + 3); //Adding 3 + 3 togther//
+
+document.write("\" Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var B = "Trying out Concatenated" + "and the String"
+document.write(B)
+
+var String= S1 ="This is the beginning of the string", S2 ="and this is the end of the string"
+document.write(S1 + S2);
