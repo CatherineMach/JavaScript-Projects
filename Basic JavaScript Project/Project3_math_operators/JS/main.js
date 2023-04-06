4c97d1fef459e065cc6b51401a03f98936ba1c1d
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-06 10:35:48 -0500

    added

---
+++ Basic JavaScript Project/Project3_math_operators/JS/main.js
@@ -0,0 +1,28 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("math") . innerHTML = "2 + 2 =" + addition;
+}
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("math").innerHTML = "5 - 2 = " + subtraction;
+}
+
+function multiplication() {
+    var simple_math = 3 * 4;
+    document,getElementById ("math").innerHTML = "3 * 4 = " + simple_math;
+}
+
+function division() {
+    var simple_math = 48 / 6;
+    document.getElementById("math").innerHTML = " 48 / 6 = " + simple_math;
+}
+
+function more_math() {
+    var simple_math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("math").innerHTML = "1 plus 2. multiplied by 10, divided in half and then subtracted by 5 equals " + simple_math;
+}
