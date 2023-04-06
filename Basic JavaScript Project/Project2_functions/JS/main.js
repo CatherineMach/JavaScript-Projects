cedcfa02e4467090804c3e2e76450aa946f65e2b
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-06 10:03:51 -0500

    made new edits

--- Basic JavaScript Project/Project2_functions/JS/main.js
+++ Basic JavaScript Project/Project2_functions/JS/main.js
@@ -2,3 +2,8 @@ var C = "testing testing testing"
 var dogs = "different dogs", Husky = "Husky", Lab = "Labrador", Poodle = "Poodle"
 document.write(C);
 document.write(Lab);
+function myFunction() {
+    var sentance = "This is me trying out operator";
+    sentence += "This is the other sentence!";
+    document.getElementById("Concatenate") .innerHTML = sentence;
+}
