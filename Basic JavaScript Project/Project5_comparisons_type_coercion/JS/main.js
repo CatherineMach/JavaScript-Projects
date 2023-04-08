cb45d36a5016bcaeecb22b66960ca7f2f6f25ed2
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-08 11:43:09 -0500

    added a bunch to project5

--- Basic JavaScript Project/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Project/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,46 @@
 document.write(typeof "Word");
 document.write(typeof 3);
 document.write("10" + 5);
+
+document.write(2E310);
+document.write(-3E310);
+document.write(10 > 2);
+document.write(10 < 2);
+console.log(2 + 2);
+console.log(10 < 2);
+document.write(10==10);
+document.write(3==11);
+
+
+X = 10;
+Y = 10;
+document.write(X === Y);
+
+M = 14;
+J = 4;
+document.write(M === J);
+
+B = 82;
+C = "82";
+document.write(B === C);
+
+G = "Magnus";
+H = "Magnus";
+document.write (G === H);
+
+//Logical Operators//
+
+document.write(5 > 2 && 10 > 4);
+document.write(5 > 10 && 10 > 4);
+document.write(5 > 10 || 10 > 4);
+document.write(5 > 10 || 10 > 20);
+
+//End Logical Operators//
+
+function not_Function() {
+    document.getElementById("Not") .innerHTML = !(20 > 10);
+}
+
+function not_Function() {
+    document.getElementById("Not2") .innerHTML = !(5 > 10);
+}
