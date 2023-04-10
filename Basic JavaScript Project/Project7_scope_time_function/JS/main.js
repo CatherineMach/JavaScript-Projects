b4d4d8564221abd9c826ec1d9c92289b5b0ddf14
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-10 13:05:12 -0500

    fixed and updated

---
+++ Basic JavaScript Project/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,35 @@
+//Global Variable//
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+//End Global//
+
+//Local Variable//
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+ }
+Add_numbers_1();
+Add_numbers_2();
+//End Local//
+
+//Console method//
+function Add_numbers_1() {
+    var X = 10;
+    console.log(15 + X);
+}
+function Add_numbers_2() {
+    console.log(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+//End Console Method//
