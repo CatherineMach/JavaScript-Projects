8ed32635b254a6207d1d97e34f4266e8aa58c59e
CatherineMach <126629434+CatherineMach@users.noreply.github.com>
2023-04-10 14:34:44 -0500

    added

--- Basic JavaScript Project/Project8_string_methods/JS/main.js
+++ Basic JavaScript Project/Project8_string_methods/JS/main.js
@@ -7,12 +7,39 @@ function slice_Method() {
 //End Slice() Method//
 
 //UpperCase Method//
-let text = "Hello World!";
-let result = text.toUpperCase();
-document.getElementById("UpperCase").innerHTML = result;
+let text1 = "Hello World!";
+let result1 = text1.toUpperCase();
+document.getElementById("UpperCase").innerHTML =result;
 //End UpperCase Method//
 
 //Search() Method//
-let text = "Mr. Blue has a blue house"
-let position = text.search("Blue");
+let text2 = "Mr. Blue has a blue house"
+let position = text2.search("Blue");
 document.getElementById("Search").innerHTMl = position;
+//End Search() Method//
+
+//Number Method//
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string") .innerHTML = X.toString();
+}
+//End Number Method//
+
+//ToPrecision Method//
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+//End ToPrecision Method//
+
+//tofixed method//
+let num = 5.56789;
+let n = num.toFixed();
+document.getElementById("Fixed").innerHTML = n;
+//End tofixed method//
+
+//valueOf() method//
+let text3 = "Hello World!";
+let result2 = text.valueOf();
+document.getElementById("value").innerHTML = result;
+//end valueOF() method//
